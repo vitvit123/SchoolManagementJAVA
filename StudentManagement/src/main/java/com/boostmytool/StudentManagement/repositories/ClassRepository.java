f9ce2a9d376a4f82b751aa5e8a197d30f260d7f7
Devit <vit123aib@gmail.com>
2024-03-06 15:36:26 +0700

    commit data

--- StudentManagement/src/main/java/com/boostmytool/StudentManagement/repositories/ClassRepository.java
+++ StudentManagement/src/main/java/com/boostmytool/StudentManagement/repositories/ClassRepository.java
@@ -2,7 +2,6 @@ package com.boostmytool.StudentManagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.boostmytool.StudentManagement.models.Class;
-
 public interface ClassRepository extends JpaRepository<Class, Long> {
    
 }
